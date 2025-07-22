2b663f3e89adb0c58781642331f2ae1fbd765e80
Kiseshik <ikuzavlev@inbox.ru>
2025-07-22 21:37:20 +0300

    Add selection sort

--- Sorts.go
+++ Sorts.go
@@ -24,3 +24,16 @@ func BubbleSort[T cmp.Ordered](slice []T) {
 		}
 	}
 }
+
+func SelectionSort[T cmp.Ordered](slice []T) {
+	n := len(slice)
+	for i := 0; i < n-1; i++ {
+		minIdx := i
+		for j := i + 1; j < n; j++ {
+			if slice[j] < slice[minIdx] {
+				minIdx = j
+			}
+		}
+		slice[i], slice[minIdx] = slice[minIdx], slice[i]
+	}
+}
